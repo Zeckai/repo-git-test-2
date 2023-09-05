6adb2ec04b98fbd924404d099af44846e5f8b871
Dzaky <m.zaky18092005@gmail.com>
2023-09-05 11:57:12 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1> Holla Sekai !</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis numquam quod quia fugiat fugit dolores nostrum suscipit, reiciendis velit deserunt dignissimos doloremque facilis tempore autem ipsa, soluta, iure aperiam ex?</p>
 </body>
 </html>
